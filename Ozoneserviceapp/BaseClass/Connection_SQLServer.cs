deb2c4fce4a4d7af95e1e182cb3374c1e65e8085
champyuyee <l2etire_yuyee@hotmail.com>
2016-08-13 11:43:27 +0700

    Add , Update
    Add : Connection_SQLServer
    Update : Login

---
+++ Ozoneserviceapp/BaseClass/Connection_SQLServer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Ozoneserviceapp.BaseClass
+{
+    public class Connection_SQLServer
+    {
+        SqlConnection sqlCon = new SqlConnection();
+       
+        public Connection_SQLServer()
+        {
+            sqlCon.ConnectionString = ConfigurationManager.ConnectionStrings["Ozoneservice_dbConnectionString"].ConnectionString;
+        }
+
+        public DataTable GetUser(string user , string pass)
+        {
+            try
+            {
+                
+                DataTable dt = new DataTable();
+                sqlCon.Open();
+                string command = "select * from tbUser where Username='" + user + "' and Password='"+pass+"'";
+                SqlDataAdapter da = new SqlDataAdapter(command, sqlCon);
+                da.Fill(dt);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                HttpContext.Current.Response.Write("<script>alert('DBConnect error : '" + ex.Message + "');</script>");
+                return null;
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+    }
+}
--- Ozoneserviceapp/Login.aspx.cs
+++ Ozoneserviceapp/Login.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Ozoneserviceapp.BaseClass;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,25 +11,37 @@ namespace Ozoneservice
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        Connection_SQLServer sqlCon = null;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            sqlCon = new Connection_SQLServer(); 
         }
 
         protected void Btnlogin_Click(object sender, EventArgs e)
         {
-            string username = inputUsername.Value;
-            string password = inputPassword.Value;
-            if(username=="admin"&&password=="password@1")
-            {               
+            try
+            {
+
+                string username = inputUsername.Value;
+                string password = inputPassword.Value;
+
+                DataTable dt = sqlCon.GetUser(username, password);
+
+                if (dt.Rows.Count < 1)
+                {
+                    RegisterClientScriptBlock("OnLoad", "<script>alert('Username or Password Invalid!!')</script>");
+                    return;
+                }
+
                 Response.Redirect("Mainpage.aspx");
+
+            }
+            catch (Exception ex)
+            {
+                RegisterClientScriptBlock("OnLoad", "<script>alert('"+ex.Message.ToString()+"')</script>");
             }
         }
 
-        
-
-        
-
-      
     }
 }
