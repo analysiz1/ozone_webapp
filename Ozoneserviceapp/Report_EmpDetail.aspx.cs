cc21e3fff7c5db0f67ac63140779a70c88866683
analysiz1 <peerapon.lim@gmail.com>
2016-09-14 08:31:48 +0700

    update

---
+++ Ozoneserviceapp/Report_EmpDetail.aspx.cs
@@ -0,0 +1,118 @@
+﻿using Ozoneserviceapp.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ozoneservice
+{
+    public partial class Report2_1 : System.Web.UI.Page
+    {
+        string innerHTML;
+        Connection_SQLServer conSql = new Connection_SQLServer();
+        public DataTable dtTraining = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string empid = Request.QueryString["empid"];
+            if(empid!=null)
+            {
+                string sql = @"SELECT
+                                c.DropinCode + '-' +a.Emp_id as Empid,
+                                a.Emp_name,
+                                a.Emp_status,
+                                c.DropinName,
+                                b.RoleName
+                                FROM
+                                dbo.tbEmployee a
+                                INNER JOIN tbEmployeeRole b on a.Emp_position = b.RoleId
+                                INNER JOIN tbDropin c on a.Emp_province = c.DropinID
+                                where Emp_id = " + empid+"";
+                BindEmpdata(sql);
+
+                string sqlt = @"SELECT
+                                b.Trainning_name,
+                                COUNT(Trainning_name) as Qty
+                                FROM
+                                dbo.tbManageTrainning a
+                                inner join tbTrainning b on  a.Trainning_id = b.Trainning_id
+                                where a.Emp_id = "+empid+@" and a.Status = 1
+                                GROUP BY b.Trainning_name";
+                BinddataTrainning(sqlt);
+            }
+            else
+            {
+                Response.Redirect("/Report_Person.aspx");
+
+            }
+            
+        }
+
+        public void BindEmpdata(string sql)
+        {
+            dtTraining = conSql.SqlQuery(sql);
+            innerHTML = "";
+            foreach (System.Data.DataRow dr in dtTraining.Rows)
+            {
+                innerHTML = @"  <table  class='table table-bordered '  style='margin-left:30px;'  >
+       <tr class='headtable'>
+            <td>รหัสผู้เข้าร่วม</td>
+            <td>" + dr["Empid"].ToString() + @"</td>                                
+        </tr>                      
+         <tr class='headtable'>
+            <td>ชื่อ-นามสกุล</td>
+            <td>" +dr["Emp_name"].ToString()+@"</td>                                
+        </tr>
+        <tr class='headtable'>
+            <td>สถานะ</td>";
+                if(dr["Emp_status"].ToString()=="1"){
+            innerHTML +="<td>ปฏิบัติงาน</td>";     
+            }
+                    else
+            { innerHTML +="<td>ลาออก</td>";  }
+        innerHTML +=@"</tr>
+        <tr class='headtable'>
+            <td>ตำแหน่ง</td>
+            <td>" + dr["RoleName"].ToString() + @"</td>                                
+        </tr>
+        <tr class='headtable'>
+            <td>จังหวัด</td>
+            <td>" + dr["DropinName"].ToString() + @"</td>                                
+        </tr>       
+
+        </table>  ";
+            }
+            EmpDetail.Text = innerHTML;
+
+        }
+
+        public void BinddataTrainning(string sqlt)
+        {
+            int no = 1;
+            dtTraining = conSql.SqlQuery(sqlt);
+            string innerHTML = "";
+             innerHTML = @"<table  class='table table-hover '  style='margin-left:30px;'  >
+        <tr class='headtable'>
+            <td>ลำดับ</td>
+            <td>เรื่อง/หัวข้อ</td>
+            <td>จำนวนครั้งที่เข้าร่วมการอบรม</td>                        
+        </tr>";
+            foreach (System.Data.DataRow dr in dtTraining.Rows)
+            {
+     innerHTML +=@"<tr class='headtable'>
+                    <td>"+ no++ +@"</td>
+                    <td>" + dr["Trainning_name"].ToString() + @"</td>
+                    <td>" + dr["Qty"].ToString()  + " ครั้ง" + @"</td>                        
+                 </tr>";
+    }
+        
+
+   innerHTML+=  "</table>";
+
+            EmpTrain.Text = innerHTML;
+
+        }
+    }
+}
---
+++ Ozoneserviceapp/Report_Person.aspx.cs
@@ -0,0 +1,127 @@
+﻿using Ozoneserviceapp.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ozoneservice.Train
+{
+    public partial class Report2 : System.Web.UI.Page
+    {
+
+        Connection_SQLServer conSql = new Connection_SQLServer();
+        public DataTable dtTraining = null;
+        string TrainningID = null;
+        string innerHTML;
+        string dropinID;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string sql = @"SELECT 
+                            a.Emp_id ,
+                            e.DropinCode+'-'+a.Emp_id  as Empid,                                                      
+                            a.Emp_title,
+                            a.Emp_name,
+                            e.DropinName,          
+                            e.DropinCode as Province,
+							e.DropinCode as Dropin,         
+                            d.RoleName                                                
+                            FROM
+                            dbo.tbEmployee a                         
+                            INNER JOIN tbEmployeeRole d on a.Emp_position = d.RoleId  
+                            inner join tbDropin e on a.Emp_province = e.DropinID 
+                            where a.Emp_status = 1 ";
+            binddataEmp(sql);
+
+        }
+
+    
+       
+
+        private void redirec()
+        {
+            Response.Redirect("~/Report2-1.aspx");
+        }
+
+        protected void btnEmp_Click(object sender, EventArgs e)
+        {
+            redirec();
+        }
+
+
+
+
+        public void binddataEmp(string sql)
+        {
+            int no = 1;
+            string dropin = ddl2.Text;
+            DataTable dt = new DataTable();
+            dtTraining = conSql.SqlQuery(sql);
+            innerHTML = "";
+            innerHTML = headtable(); // headtable
+
+            foreach (System.Data.DataRow dr in dtTraining.Rows)
+            {                
+                /*Content Emp*/
+                innerHTML += "<tr>";
+                innerHTML += "<td>" + no++ + "</td>";
+                innerHTML += "<td>" + dr["Empid"].ToString() + "</td>";                
+                innerHTML += "<td>" + dr["Emp_title"].ToString() + "</td>";
+                innerHTML += "<td>" + dr["Emp_name"].ToString() + "</td>";
+                innerHTML += "<td>" + dr["Province"].ToString() + "</td>";
+                innerHTML += "<td>" + dr["Dropin"].ToString() + "</td>";
+                innerHTML += "<td>" + dr["RoleName"].ToString() + "</td>";                
+                innerHTML += "<td>";
+                innerHTML += "<input type='Button' id='btnAdd' name='" + dr["Empid"].ToString() + "' onclick='redirect("+dr["Emp_id"].ToString()+")' runat='server' value='ดูรายละเอียดการอบรม' Class='btn btn-primary' />";                               
+                innerHTML += "</td>";
+                innerHTML += "</tr>";
+
+            }
+            innerHTML += "</table>     </div>";
+            lblEmp.Text = innerHTML; // output to fontend
+        }
+
+        public string headtable()
+        {
+            string thead = @"<div class=''>
+            <table  class='table table-hover '  style='margin-left:30px;'>
+            <tr class='headtable'><!-- Headtable -->
+            <td>ลำดับ</td>
+            <td>รหัสพนักงาน</td>            
+            <td>คำนำหน้า</td>
+            <td>ชื่อ-นามสกุล</td>
+            <td>จังหวัด</td>
+            <td>สำนักงาน/พื้นที่</td>            
+            <td>ตำแหน่ง</td>
+            <td></td>
+            </tr>";
+            return thead;
+        }
+
+        protected void ddl2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sql = @"SELECT 
+                            a.Emp_id ,
+                            e.DropinCode+'-'+a.Emp_id  as Empid,                                                      
+                            a.Emp_title,
+                            a.Emp_name,
+                            e.DropinName,          
+                            e.DropinCode as Province,
+							e.DropinCode as Dropin,         
+                            d.RoleName                                                
+                            FROM
+                            dbo.tbEmployee a                         
+                            INNER JOIN tbEmployeeRole d on a.Emp_position = d.RoleId  
+                            inner join tbDropin e on a.Emp_province = e.DropinID 
+                            where a.Emp_status = 1 and e.DropinID = " + ddl2.SelectedValue+" ";
+            binddataEmp(sql);
+        }
+
+
+
+    }
+}
