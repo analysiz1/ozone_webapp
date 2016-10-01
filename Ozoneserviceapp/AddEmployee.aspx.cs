ba024e18ff1320c1deb16c412f44579bc54b2cfe
Peerapon <peerapon.lim@gmail.com>
2016-10-01 15:16:32 +0700

    update 1/10/2559
    AddEmp

--- Ozoneserviceapp/AddEmployee.aspx.cs
+++ Ozoneserviceapp/AddEmployee.aspx.cs
@@ -22,17 +22,18 @@ namespace Ozoneserviceapp
         protected void Btnadd_Click(object sender, EventArgs e)
         {
             string Title = Ddtitle.SelectedItem.ToString();
-            string Empname = Txtnname.ToString();
+            string Empname = Txtnname.Text;
             string role = Ddrole.SelectedValue;
             string dropin = Dddropin.SelectedValue;
 
-            string sql = "insert into tbEmployee(Emp_title,Emp_name,Emp_position,Emp_province) values(' "+Title.ToString()+"','"+Empname.ToString()+"','"+role+"','"+dropin+"')";
-
+            string sql = "INSERT INTO tbEmployee(Emp_id,Emp_title,Emp_name,Emp_position,Emp_province,Emp_status) values((select max(Emp_id) from tbEmployee)+1 ,' " + Title.ToString() + "','" + Empname.ToString() + "','" + role + "','" + dropin + "',1)";         
             conSql.ExcuteSql(sql);
 
-            Console.Write("Add Complete");
 
 
+            ClientScript.RegisterStartupScript(GetType(), "hwa", "alert('Complete');", true);
+
+           
 
         }
     }
