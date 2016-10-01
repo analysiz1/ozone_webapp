7762472568ff56e9ac3a72f5c3adde2f9b831ce5
Peerapon <peerapon.lim@gmail.com>
2016-10-01 14:47:19 +0700

    update

--- Ozoneserviceapp/AddEmployee.aspx.cs
+++ Ozoneserviceapp/AddEmployee.aspx.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Ozoneserviceapp.BaseClass;
+using System.Data;
+
 
 namespace Ozoneserviceapp
 {
     public partial class AddEmployee : System.Web.UI.Page
     {
+
+        Connection_SQLServer conSql = new Connection_SQLServer();
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,6 +21,18 @@ namespace Ozoneserviceapp
 
         protected void Btnadd_Click(object sender, EventArgs e)
         {
+            string Title = Ddtitle.SelectedItem.ToString();
+            string Empname = Txtnname.ToString();
+            string role = Ddrole.SelectedValue;
+            string dropin = Dddropin.SelectedValue;
+
+            string sql = "insert into tbEmployee(Emp_title,Emp_name,Emp_position,Emp_province) values(' "+Title.ToString()+"','"+Empname.ToString()+"','"+role+"','"+dropin+"')";
+
+            conSql.ExcuteSql(sql);
+
+            Console.Write("Add Complete");
+
+
 
         }
     }
